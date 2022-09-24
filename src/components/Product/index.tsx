859940f1df7ce2e4cd2ac099f9764fd07771ca5e
Joe Pena <jpenajr3@gmail.com>
2022-09-24 00:31:34 -0400

    feat: adds fonts and colors

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -10,17 +10,25 @@ const Product = (props) => {
             sku,
             price,
             images,
-        }
+        },
+        components: [{
+            content: {
+                plainText: [summary],
+            },
+        }],
     } } = props;
 
     const lowerName = name.toLowerCase().replace(' ', '-');
+    const nameArray = name.split(' ');
+    const nameStart = nameArray.shift();
+    const nameEnd = nameArray.join(' ');
 
     return (
         <div class={`product product--${lowerName}`}>
-            <div>{name}</div>
-            <div>{sku}</div>
-            <div>${price}.00</div>
-            {images?.map(({url}) => <img src={url} />)}
+            <h2 class="product__title">{nameStart} <span class="product__title--end text--zighead">{nameEnd}</span></h2>
+            <div class="product__price">${price}.00</div>
+            {images?.map(({url}) => <img class="product__img" src={url} />)}
+            <div class="product__summary">{summary}</div>
         </div>
     );
 }
